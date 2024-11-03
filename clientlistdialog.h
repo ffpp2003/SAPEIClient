bff762886a3f2ef65123e3cfca42c1a73ab62c76
gaston1709 <gaston.grasso.bowser@gmail.com>
2024-11-03 19:52:40 -0300

    fix-issue-#32

--- clientlistdialog.h
+++ clientlistdialog.h
@@ -2,8 +2,12 @@
 #define CLIENTLISTDIALOG_H
 
 #include <QDialog>
+#include <QMessageBox>
+#include <QString>
+#include "lib/SAPEICore/Client.h"
 #include "lib/SAPEICore/DataBase.h"
 #include "vehiclelistdialog.h"  // Incluir el nuevo diálogo de vehículos
+#include "editclientdialog.h"
 
 
 namespace Ui {
@@ -22,7 +26,8 @@ private slots:
     void updateClientList();               // Muestra los nombres de los clientes en la lista
     void onSearchTextChanged(const QString &text);  // Filtra los clientes por nombre
     void onClientDoubleClicked(QListWidgetItem* item);  // Slot para doble clic en cliente
-
+    void onEditClientButtonClicked();
+    void onDeleteClientButtonClicked();
 private:
     Ui::ClientListDialog *ui;
     DataBase *database;
---
+++ editclientdialog.cpp
@@ -0,0 +1,67 @@
+#include "editclientdialog.h"
+#include "ui_editclientdialog.h"
+
+EditClientDialog::EditClientDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::EditClientDialog)
+{
+    ui->setupUi(this);
+
+    connect(ui->saveButton, &QPushButton::clicked, this, &QDialog::accept);
+    connect(ui->cancelButton, &QPushButton::clicked, this, &QDialog::reject);
+}
+
+EditClientDialog::~EditClientDialog()
+{
+    delete ui;
+}
+
+// Métodos para obtener los datos del cliente
+QString EditClientDialog::getName() const {
+    return ui->nameLineEdit->text();
+}
+
+QString EditClientDialog::getDNI() const {
+    return ui->dniLineEdit->text();
+}
+
+QString EditClientDialog::getEmail() const {
+    return ui->emailLineEdit->text();
+}
+
+QString EditClientDialog::getPhone() const {
+    return ui->phoneLineEdit->text();
+}
+
+QString EditClientDialog::getAddress() const {
+    return ui->addressLineEdit->text();
+}
+
+int EditClientDialog::getAge() const {
+    return ui->ageSpinBox->value();
+}
+
+// Métodos para establecer los datos del cliente
+void EditClientDialog::setName(const QString &name) {
+    ui->nameLineEdit->setText(name);
+}
+
+void EditClientDialog::setDNI(const QString &dni) {
+    ui->dniLineEdit->setText(dni);
+}
+
+void EditClientDialog::setEmail(const QString &email) {
+    ui->emailLineEdit->setText(email);
+}
+
+void EditClientDialog::setPhone(const QString &phone) {
+    ui->phoneLineEdit->setText(phone);
+}
+
+void EditClientDialog::setAddress(const QString &address) {
+    ui->addressLineEdit->setText(address);
+}
+
+void EditClientDialog::setAge(int age) {
+    ui->ageSpinBox->setValue(age);
+}
